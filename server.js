8de714c4721fba055a22ce303ee11f54461beca4
a_ira0011 <ryomaog1234@gmail.com>
2026-02-18 16:36:54 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,33 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 
 const proxy = httpProxy.createProxyServer({
-  changeOrigin: true, 
-  autoRewrite: true,   
+  target: 'https://www.youtube.com', // ログインしたいサイトのURL
+  changeOrigin: true,            // ホストヘッダーをターゲットに合わせる
+  autoRewrite: true,             // リダイレクト先をプロキシURLに書き換える
+  xfwd: true,                    // X-Forwarded-For ヘッダーを追加
+  cookieDomainRewrite: "",       // 重要：Cookieの送信先ドメインをプロキシに変更
+  preserveHeaderKeyCase: true    // ヘッダーの大文字小文字を維持
 });
 
-const targetUrl = 'https://www.youtube.com'; 
-
 const server = http.createServer((req, res) => {
-  
-  proxy.web(req, res, { target: targetUrl }, (e) => {
-    console.error('Proxy Error:', e);
-    res.statusCode = 502;
+  // エラーハンドリング
+  proxy.web(req, res, {}, (err) => {
+    console.error('Proxy Error:', err);
+    res.writeHead(502);
     res.end('Proxy Error');
   });
 });
 
-console.log(`Proxy server is running. Target: ${targetUrl}`);
-server.listen(3000);
+// セッション維持のためにCookieヘッダーを調整する設定
+proxy.on('proxyRes', function (proxyRes, req, res) {
+  // ブラウザからのCookieを受け入れ可能にする処理
+  if (proxyRes.headers['set-cookie']) {
+    proxyRes.headers['set-cookie'] = proxyRes.headers['set-cookie'].map(cookie => 
+      cookie.replace(/Domain=[^;]+;?/, '') // ターゲットドメインの制限を解除
+    );
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Login-ready proxy running on http://localhost:3000');
+});
