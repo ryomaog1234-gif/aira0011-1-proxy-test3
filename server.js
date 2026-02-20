18390d38f2693c78716ed4b9915b7bac45a5c409
a_ira0011 <ryomaog1234@gmail.com>
2026-02-20 16:28:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 
 const proxy = httpProxy.createProxyServer({
-  target: 'https://minefun.io', // ログインしたいサイトのURL
+  target: 'https://racinglimits.io/', // ログインしたいサイトのURL
   changeOrigin: true,            // ホストヘッダーをターゲットに合わせる
   autoRewrite: true,             // リダイレクト先をプロキシURLに書き換える
   xfwd: true,                    // X-Forwarded-For ヘッダーを追加
